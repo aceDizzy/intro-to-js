0877a212ef7772cbf9fb451fe6f9a6c3af2f86f8
Ace Delacrois <93988552+aceDizzy@users.noreply.github.com>
2022-05-27 09:04:23 +0800

    Create code-magnet.js

---
+++ code-magnet.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Code Magnets</h2>
+
+<p>Head First JavaScript Programming Coding Exercise</p>
+
+<p id="demo"></p>
+
+<script>
+var name = "Joe";
+var i = 0;
+while (i<2) {
+document.write("Happy Birthday to you. <br>");
+i = i + 1;
+}
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you.<br>");
+
+</script>
+
+</body>
+</html>
