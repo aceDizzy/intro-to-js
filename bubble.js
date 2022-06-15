84f024a0738633637fb18cb292c2976108433f98
Ace Delacrois <93988552+aceDizzy@users.noreply.github.com>
2022-06-15 09:51:25 +0800

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -32,3 +32,25 @@ console.log("Highest bubble score: " + highScore);
 
 var bestSolutions = getBestResults(scores,highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
+
+var costs = [.25, .27, .25, .25, .25, .25,
+             .33, .31, .25, .29, .27, .22,
+             .31, .25, .25, .33, .21, .25,
+             .25, .25, .28, .25, .24, .22,
+             .20, .25, .30, .25, .24, .25,
+             .25, .25, .27, .25, .26, .29];
+
+function getMostCostEffectiveSolution(scores, costs, highScore) {
+    var cost = 100;
+    var index;
+    
+    for (var  i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore && cost > costs[i] ) {
+            index = i;
+            cost = costs[i];
+        }
+    }
+    return index;
+}
+var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log("Bubble solution #" + mostCostEffective + " is the most cost effective");
